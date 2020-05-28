dfdf0c7fc3fbd7fe76de2a289c5030b6a5e396b2
Aleksey <42493212+catWebua@users.noreply.github.com>
2020-05-28 07:52:32 +0300

    Add files via upload

---
+++ site/js/common.js
@@ -0,0 +1,10 @@
+$('.owl-carousel').owlCarousel({
+    items: 1,
+    margin: 0,
+    loop: true,
+    dots: true,
+    nav: false,
+    autoplay : true,
+    autoplayTimeout : 5000,
+    smartSpeed: 1000
+});
